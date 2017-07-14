c0c6024066dfb7f6e6e4c3e707b225018f897c13
GrapeSoftCN <jk282@163.com>
2017-07-14 19:11:24 +0800

    修改

---
+++ src/model/FileImg.java
@@ -0,0 +1,41 @@
+package model;
+
+public class FileImg {
+	public static String getIcon(String extName) {
+		String out = "icon\\";
+		extName = extName.toLowerCase();
+		switch (extName) {
+		case "doc":
+		case "docx":
+			out = out+"word.ico";
+			break;
+		case "xls":
+		case "xlsx":
+			out = out+"excel.ico";
+			break;
+		case "ppt":
+			out = out+"PowerPoint.ico";
+			break;
+		case "txt":
+			out = out+"txt.ico";
+			break;
+		case "htm":
+		case "html":
+			out = out+"html.ico";
+			break;
+		case "pdf":
+			out = out+"pdf.ico";
+			break;
+		case "mp3":
+			out = out+"mp3.ico";
+			break;
+		case "wav":
+			out = out+"wav.ico";
+			break;
+		default:
+			out = out+"other.ico";
+			break;
+		}
+		return out;
+	}
+}
